aa2c9a218764fab3d1665c5bf45cc9c627028609
hx grx <hx_grx@hx-grx.local>
2017-08-18 15:11:39 +0800

    Initial Commit

---
+++ iosDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosDemo
+//
+//  Created by hx_grx on 2017/8/18.
+//  Copyright © 2017年 hx_grx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iosDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iosDemo
+//
+//  Created by hx_grx on 2017/8/18.
+//  Copyright © 2017年 hx_grx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
